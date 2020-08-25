1b474c55454092762607bc6ec4447e1751066994
sub-s <spoon718@gmail.com>
2020-08-25 16:59:23 +0900

    ㅇㄹㅎㄹㅎ

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, SafeAreaView, FlatList} from 'react-native';
 
 const styles = StyleSheet.create({
   body: {
@@ -10,14 +10,56 @@ const styles = StyleSheet.create({
 });
 
 
+const dd = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title, id }) => (
+  <View>
+    <Text>{title}</Text>
+    <Text>{id}</Text>
+  </View>
+);
+
+
 const MainIndex = ({navigation}) => {
+
+    const renderItem = ({ item }) => (
+      <Item title={item.title} id={item.id} />
+    );
+
+
   return (
+
     <View style={styles.body}>
-      <Text>메인</Text>
+      <Text>{dd[0].id}</Text>
       <Button
         title="이동하자"
         onPress={() => navigation.navigate('MainSub')}
       />
+
+      <FlatList
+
+        data={dd}
+        renderItem={renderItem}
+
+        id={item => item.id}
+        title={item => item.title}
+      />
+
+
+
     </View>
   );
 };
